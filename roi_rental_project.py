231c13588612cbbab80cd7079aed4ecf50c43ae3
cstevenson7 <cindy.stevenson1@gmail.com>
2020-08-01 13:23:02 -0600

    Full working version

--- roi_rental_project.py
+++ roi_rental_project.py
@@ -1,21 +1,17 @@
-# class Property():
-#     def __init__(self,price,rating):
-        # self.price = price
-        # self.rating = rating
+import sys
 
 class Rental():
     #initialize the class and the constructed values
-    def __init__(self, cost, incomes, expenses):
-        self.cost = cost
+    def __init__(self, incomes, expenses):      
         self.incomes = incomes
         self.expenses = expenses
         """
-            Rental requires 3 positional arguments
-            cost - the building purchase price - int expected
+            Rental requires positional arguments:                     
             income - monthly income of building - dictionary expected
             expenses - monthly expenses of building -  dictionary expected
 
         """
+
     # Method to display the monthly expense or the monthly income dictionaries
     def showDictionary(self, type):
         if type == 'income':
@@ -32,7 +28,8 @@ class Rental():
                 print(f"\nHere is what you have for the monthly {type}.")
                 for key,value in self.expenses.items():
                     print(key,":",value)
-    
+
+
     #Method to add monthly incomes
     def addIncome(self):      
         while True:
@@ -50,17 +47,15 @@ class Rental():
             else: 
                  print("\nSorry, that is not a vaild choice for income type, try again")
                  continue
-   
 
 
-    
     #Method to add monthly expenses
     def addExpense(self):         
         while True:
             print("\nExpense types are:")
             print("Mortgage, Capital Expenditures, Taxes, Insurance, Utilities, Vacancy, Home Owners Association") 
             print("Yard Service, Repairs, Property Management, Other")
-            exp_name = input("\n Enter expense type and enter quit when you are done: ")
+            exp_name = input("\nEnter expense type and enter quit when you are done: ")
             if exp_name.lower().strip() == "quit" or exp_name.lower().strip() == "q":
                 break
             if exp_name.lower().strip() == 'mortgage' or exp_name.lower().strip() == 'capital expenditures' or  exp_name.lower().strip() == 'taxes' or  exp_name.lower().strip()  == 'insurance' or  exp_name.lower().strip()  == 'utilities' or  exp_name.lower().strip()  == 'vacancy' or  exp_name.lower().strip()  == 'home owners associaition' or  exp_name.lower().strip()  == 'yard service' or  exp_name.lower().strip()  == 'repairs' or  exp_name.lower().strip()  == 'property management' or  exp_name.lower().strip()  == 'other':     
@@ -73,12 +68,15 @@ class Rental():
             else: 
                  print("\nSorry, that is not a vaild choice for expense type, try again.")
                  continue
-    #Method to sum up Income 
+
+
+    #Method to sum up income 
     def sumIncome(self):
         total_income = 0
         for value in self.incomes.values():
             total_income += value
         return total_income
+
 
     # #Method to sum up Expenses
     def sumExpenses(self):
@@ -87,6 +85,8 @@ class Rental():
             total_expenses += value
         return total_expenses
 
+
+    #Method to calculate cask flow
     def calculateCashFlow(self):
         #total_income = self.sumIncome()
         print(f"\nTotal monthly income = ${self.sumIncome():,}")
@@ -96,6 +96,8 @@ class Rental():
         print(f"\nTotal Monthly Cash Flow = ${cash_flow:,}")
         return cash_flow
 
+
+    #Method to chrck user input
     def inputCheck(self, name =""):
         while True:
                 try:
@@ -105,9 +107,11 @@ class Rental():
                     print("\nPlease enter a whole number.") 
         return user_input 
 
-    
+
+    #Method to calculate the ROI
     def calculateROI(self, monthly_cash_flow):
-        print("\nJust about done! Now we need your cash expenditures")
+        print("\nJust about done!\n")
+        bldg_cost = self.inputCheck('building')
         down_payment = self.inputCheck('down payment')
         closing = self.inputCheck('closing costs')
         rehab = self.inputCheck('rehab costs')
@@ -115,16 +119,18 @@ class Rental():
         total_cash_exp = down_payment + closing + rehab + other_costs
         annual_cash_flow = monthly_cash_flow * 12
         roi = annual_cash_flow / total_cash_exp * 100        
-        print (f" \nYour Cash on Cash ROI for this building is {roi} %")
+        print (f" \nYour Cash on Cash ROI for the ${bldg_cost:,} building is {roi} % ")
 
-    #Method for user check entries  and start again
+
+    #Method for user for check entries and start again if errors
     def checkDictionary(self, type):
         if type == 'income':
             while True:
                 ck_income = input("Are your income entries correct? y/n : ")
                 if ck_income.lower() == 'n':
                     self.clearCalculator()
-                    print ("The calculator has been cleared, please start again")
+                    print ("\nThe calculator has been cleared, please start again\n")
+                    sys.exit(0)
                 elif ck_income.lower() == 'y':
                     break
                 else:
@@ -134,7 +140,8 @@ class Rental():
                 ck_exp = input("Are your expense entries correct? y/n : ")
                 if ck_exp.lower() == 'n':
                     self.clearCalculator()
-                    print("The calculator has been cleared, please start again")
+                    print("\nThe calculator has been cleared, please start again\n")
+                    sys.exit(0)
                 elif ck_exp.lower() == 'y':
                     break
                 else:
@@ -148,15 +155,16 @@ class Rental():
         self.expenses.clear()    
 
 
-
-
 def run():
-    #creating building object which is an instance of the Rental class
-    building = Rental(0,{},{})
+    
+    #Creating building object which is an instance of the Rental class
+    building = Rental({},{})
  
     print("\nWelcome to the Cash on Cash Return on Investment Calculator!")
     print( "This calculator uses the Four Square Method. Please round numbers to nearest dollar.\nDisclaimer: use at own risk")
-           
+
+
+
     #Add Monthly Income
     print("\nFirst you will enter the monthly income of your building:")                
     building.addIncome()
@@ -167,9 +175,8 @@ def run():
     print("\nNext you will enter the monthly expenses of your building:")                
     building.addExpense()
     building.showDictionary('expense')
-    # clear_req = input("\n Do you want to clear the calculator and start again?  Enter yes or no: ")
-    # if clear_req.lower() == "yes":
-    #     building.clearCalculator()
+    building.checkDictionary('expense')
+
 
     # Calculate Cash Flow and ROI
     building.calculateROI(building.calculateCashFlow())
